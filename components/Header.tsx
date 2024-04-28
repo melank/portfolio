25936f8e3de1b84ff88ce54d5da21b537dda6fa1
melank <masaki.vr@gmail.com>
2024-04-28 23:35:29 +0900

    styled-componentsをhono cssに変換

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import { FC } from "hono/jsx"; 
+import { css } from 'hono/css';
+
+export const Header: FC = () => {
+  const styledHeader = css`
+    color: rgb(250, 220, 0);
+    padding: 10px;
+    background: #343b3f;
+    width: 100%;
+  `;
+
+  const styledHeaderLink = css`
+    text-decoration: none;
+    color: white;
+    margin-left: 10px;
+  `;
+
+  const styledTitle = css`
+    font-size: 24px;
+  `;
+
+  return (
+    <header class={styledHeader}>
+      <span class={styledTitle}>melank.me</span>
+      <a class={styledHeaderLink} href="https://github.com/melank">
+        GitHub
+      </a>
+      <a class={styledHeaderLink} href="https://wantedly.com/id/melank">
+        Wantedly
+      </a>
+      <a class={styledHeaderLink} href="https://techtrain.dev/mentors/221">
+        TechTrain
+      </a>
+    </header>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,34 @@
-import { Hono } from "hono"
+import { Hono } from 'hono' 
+import { Header } from '../components/Header';
+import { MyIcon } from '../components/MyIcon';
+import { css } from 'hono/css'
 
 const app = new Hono()
 
-app.get('/', (c) => c.html('Hello, World!'))
+// div
+const container = css`
+  margin: 20px 20px 120px 20px;
+`;
+// header
+const styledFooter = css`
+  color: #fff;
+  text-align: center;
+  padding: 10px 0;
+  color: rsgb(250, 220, 0);
+  background: #343b3f;
+  width: 100%;
+  position: fixed; /* 要素の位置を固定する */
+  bottom: 0; /* 基準の位置を画面の一番下に指定する */
+`;
+// p
+const styledParagraph = css`
+  margin-bottom: 8px;
+`;
+
+app.get('/', (c) => c.html(
+  <Header>
+    <MyIcon src={"images/icon.png"}></MyIcon>
+  </Header>
+)) 
 
 export default app
